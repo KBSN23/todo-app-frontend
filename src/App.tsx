c16cf44e9e98441a9e5c92e4598bb6c9f767f72e
JKordyka <j.kordyka@ap-tech.pl>
2025-08-29 18:00:36 +0200

    update eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 const App = () => {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind World</h1>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind World
+      </h1>
+    </div>
+  );
 };
 
 export default App;
