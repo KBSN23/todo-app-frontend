a451feaba3c17b1f755b4fb5fcfe6f8a2363e03b
KBSN23 <124698486+KBSN23@users.noreply.github.com>
2025-08-29 19:25:30 +0200

    Setup zustand (#14)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { Tasks } from "./tasks";
+
 const App = () => {
+  const { actions, tasks } = Tasks.Store.useTaskStore();
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
         Hello, Tailwind World
       </h1>
+      <div>
+        {tasks.map((task) => (
+          <div key={task}>{task}</div>
+        ))}
+      </div>
+      <div>
+        <button
+          className="px-4 py-2 mt-4 font-semibold text-white bg-blue-500 rounded hover:bg-blue-700"
+          onClick={() => actions.addTask("New Task")}
+        >
+          Add task
+        </button>
+        <button
+          className="px-4 py-2 mt-4 font-semibold text-white bg-red-500 rounded hover:bg-red-700"
+          onClick={() => actions.removeTask("New Task")}
+        >
+          Remove task
+        </button>
+      </div>
     </div>
   );
 };
---
+++ src/tasks/private/index.ts
@@ -0,0 +1,3 @@
+export * as Store from "./tasks.store";
+export * as Types from "./tasks.types";
+export * as Hooks from "./tasks.hooks";
