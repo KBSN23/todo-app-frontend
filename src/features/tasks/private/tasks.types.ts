ac38745a9a96b8847f709ae2474d2f233ddb8f63
JKordyka <j.kordyka@ap-tech.pl>
2025-08-31 21:32:05 +0200

    Update features

--- src/features/tasks/private/tasks.types.ts
+++ src/features/tasks/private/tasks.types.ts
@@ -1,8 +1,28 @@
+import z from "zod";
+
+export const TaskStatusSchema = z.enum(["pending", "in_progress", "done"]);
+export type TaskStatus = z.infer<typeof TaskStatusSchema>;
+
+export const TaskPrioritySchema = z.enum(["low", "medium", "high"]);
+export type TaskPriority = z.infer<typeof TaskPrioritySchema>;
+export const TaskSchema = z.object({
+  id: z.uuid(),
+  title: z.string().min(2).max(100),
+  description: z.string().max(500).optional(),
+  status: TaskStatusSchema.default("pending"),
+  priority: TaskPrioritySchema.default("medium"),
+  createdAt: z.coerce.date().optional(),
+  dueDate: z.coerce.date().optional(),
+  categoryIds: z.array(z.uuid()).optional(),
+});
+
+export type Task = z.infer<typeof TaskSchema>;
+
 export type State = {
-  tasks: string[];
+  tasks: Task[];
 };
 
 export type Actions = {
-  addTask: (task: string) => void;
-  removeTask: (task: string) => void;
+  addTask: (task: Omit<Task, "id" | "createdAt">) => void;
+  removeTask: (task: Task["id"]) => void;
 };
